2146c21cca103d2d437a7163d004d8b82b7803fa
Micael Picco <micaelpicco@mbp-de-micael.home>
2022-01-13 17:59:02 -0300

    prueba2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'micael';
+const nuevaString = 'micaell';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 22;
 
 // Crea una variable booleana:
 const nuevoBool = false;
